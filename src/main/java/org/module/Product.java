6d2c9249be6c7edb9078566e78672abc7e0f35b6
Josh <joshuastolle445@gmail.com>
2023-06-09 09:56:26 +0200

    product class body

--- src/main/java/org/module/Product.java
+++ src/main/java/org/module/Product.java
@@ -1,4 +1,12 @@
 package org.module;
 
 public class Product {
+    int id;
+    String name;
+
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
 }
