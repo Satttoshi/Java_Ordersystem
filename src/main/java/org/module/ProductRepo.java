d223b6bdb02609b5521bb9301c8ac642d5f5a212
Josh <joshuastolle445@gmail.com>
2023-06-09 10:05:17 +0200

    add product repo body

--- src/main/java/org/module/ProductRepo.java
+++ src/main/java/org/module/ProductRepo.java
@@ -1,4 +1,11 @@
 package org.module;
 
+import java.util.List;
+
 public class ProductRepo {
+    List<Product> products;
+
+    public ProductRepo(List<Product> products) {
+        this.products = products;
+    }
 }
