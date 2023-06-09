5763d772936e246affa31d24ce6429edb3462eed
Josh <joshuastolle445@gmail.com>
2023-06-09 10:03:36 +0200

    Order class body

--- src/main/java/org/module/Order.java
+++ src/main/java/org/module/Order.java
@@ -1,4 +1,14 @@
 package org.module;
 
+import java.util.List;
+
 public class Order {
+    int id;
+    List<Product> products;
+
+    public Order(int id, List<Product> products) {
+        super();
+        this.id = id;
+        this.products = products;
+    }
 }
