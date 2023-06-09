fefb6bfdd5b583b6b24f081ab5cdc8024998b3f4
Josh <joshuastolle445@gmail.com>
2023-06-09 11:40:46 +0200

    add userChoice terminal handler method

--- src/main/java/org/module/Main.java
+++ src/main/java/org/module/Main.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
 
         ShopService shopService = new ShopService(List.of(
             new Product(0, "Nike Shoes"),
@@ -16,11 +18,33 @@ public class Main {
         ));
 
         System.out.println("Welcome to the shop!");
-        System.out.println("What do you want to do? :");
+        System.out.println("Do you want to view a Product?");
+        if(userChoice()) {
+            System.out.println("Enter the ID of the product you want to view: ");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println(shopService.getProduct(id));
+        }
+
+
 
 
         System.out.println(shopService.getProducts());
 
 
+    }
+
+    public static boolean userChoice() {
+        System.out.println("Yes or No?: ");
+        String choice = scanner.nextLine();
+        String choiceLower = choice.toLowerCase();
+        if (choiceLower.equals("yes") || choiceLower.equals("y")) {
+            return true;
+        } else if (choiceLower.equals("no") || choiceLower.equals("n")) {
+            return false;
+        } else {
+            System.out.println("Invalid input, please try again.");
+            return userChoice();
+        }
     }
 }
